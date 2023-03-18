da32eadebbbacd859718c63a51322936c0da60aa
noidnoid <88474228+noidnoid@users.noreply.github.com>
2023-03-18 16:22:48 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -16,3 +16,14 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/Meroser/luci-app-aria2 package/luci-app-aria2
+git clone https://github.com/krystic/luci-app-cifs-mount package/luci-app-cifs-mount
+git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
+
+
+
+echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
+echo 'src-git https://github.com/Meroser/luci-app-aria2' >>feeds.conf.default
+echo 'src-git https://github.com/krystic/luci-app-cifs-mount' >>feeds.conf.default
+echo 'src-git https://github.com/sirpdboy/luci-app-ddns-go' >>feeds.conf.default
